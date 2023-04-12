3a829849610a80901b87c669a3aa5b8c1ddba74f
l00ma <looma@protonmail.ch>
2023-04-12 14:31:21 +0200

    first version of content

--- src/routes.php
+++ src/routes.php
@@ -7,10 +7,6 @@
 //          3. (optional) array of query string keys to send as parameter to the method
 // e.g route '/item/edit?id=1' will execute $itemController->edit(1)
 return [
-    '' => ['HomeController', 'index',],
-    'items' => ['ItemController', 'index',],
-    'items/edit' => ['ItemController', 'edit', ['id']],
-    'items/show' => ['ItemController', 'show', ['id']],
-    'items/add' => ['ItemController', 'add',],
-    'items/delete' => ['ItemController', 'delete',],
+    '' => ['ItemController', 'index',],
+    'add' => ['ItemController', 'add',],
 ];
