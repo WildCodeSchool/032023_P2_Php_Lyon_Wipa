6e6a46b5f6497262b1c172a58ec8578671f55a96
AurelOconnell <aurelienoconnell@gmail.com>
2023-04-12 20:10:01 +0200

    minor add method changement

--- src/Controller/ItemController.php
+++ src/Controller/ItemController.php
@@ -65,15 +65,10 @@ class ItemController extends AbstractController
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $item = array_map('trim', $_POST);
 
-            if (!isset($item['picture']) || empty($item['picture'])) {
-                $errors[] = 'You must enter an URL';
-            }
-            if (!filter_var($item['picture'], FILTER_VALIDATE_URL)) {
-                $errors[] = 'Wrong URL format';
-            }
-            if (!isset($item['prompt']) || empty($item['prompt']) || !isset($item['description']) || empty($item['description'])) {
-                $errors[] = 'You must write a prompt and a comment';
-            }
+            $this->validateURL($item, $errors);
+            $this->validatePrompt($item, $errors);
+            $this->validateComment($item, $errors);
+            
             if (empty($errors)) {
                 $itemManager = new ItemManager();
                 $id = $itemManager->insert($item);
@@ -84,6 +79,30 @@ class ItemController extends AbstractController
         }
 
         return $this->twig->render('Item/add.html.twig', ['errors' => $errors]);
+    }
+
+    public function validateURL(array $item, array $errors): void
+    {
+        if (!isset($item['picture']) || empty($item['picture'])) {
+            $errors[] = 'You must enter an URL';
+            return;
+        }
+        if (!filter_var($item['picture'], FILTER_VALIDATE_URL)) {
+            $errors[] = 'Wrong URL format';
+        }
+    }
+
+    public function validatePrompt(array $items, array $errors): void 
+    {
+        if (!isset($item['prompt']) || empty($item['prompt'])) {
+            $errors[] = 'You must write a prompt';
+        }
+    }
+    public function validateComment(array $items, array $errors): void 
+    {
+        if (!isset($item['description']) || empty($item['description'])) {
+            $errors[] = 'You must write a comment';
+        }
     }
 
     /**
