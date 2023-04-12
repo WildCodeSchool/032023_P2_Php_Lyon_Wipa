cd2056d25431f4463ba55c69de56e364669f5086
l00ma <looma@protonmail.ch>
2023-04-12 14:31:21 +0200

    first version of content

--- src/Model/ItemManager.php
+++ src/Model/ItemManager.php
@@ -6,15 +6,18 @@ use PDO;
 
 class ItemManager extends AbstractManager
 {
-    public const TABLE = 'item';
+    public const TABLE = 'photo';
 
     /**
      * Insert new item in database
      */
     public function insert(array $item): int
     {
-        $statement = $this->pdo->prepare("INSERT INTO " . self::TABLE . " (`title`) VALUES (:title)");
-        $statement->bindValue('title', $item['title'], PDO::PARAM_STR);
+        $statement = $this->pdo->prepare("INSERT INTO " . self::TABLE . " (`photo_title`,`photo_link`, `photo_prompt`, `photo_description`) VALUES (:photo_title, :photo_link, :photo_prompt, :photo_description)");
+        $statement->bindValue('photo_title', $item['photo_title'], PDO::PARAM_STR);
+        $statement->bindValue('photo_link', $item['photo_link'], PDO::PARAM_STR);
+        $statement->bindValue('photo_prompt', $item['photo_prompt'], PDO::PARAM_STR);
+        $statement->bindValue('photo_description', $item['photo_description'], PDO::PARAM_STR);
 
         $statement->execute();
         return (int)$this->pdo->lastInsertId();
