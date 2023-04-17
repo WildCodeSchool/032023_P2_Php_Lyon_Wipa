6ae27d91a50f05f40ac76df29b000ef729c8bc6c
MisterPingouin <anthony.axeldisplay@gmail.com>
2023-04-17 19:49:45 +0200

    Add login page/Logcontroller / + Grumpy

--- src/Controller/LogController.php
+++ src/Controller/LogController.php
@@ -4,31 +4,33 @@ namespace App\Controller;
 
 class LogController extends AbstractController
 {
-  public function log()
-  {
-    define('LOGIN', 'wipa');
-    define('PASSWORD', 'admin');
-    $errorMessages = [];
+    public function log()
+    {
+        define('LOGIN', 'wipa');
+        define('PASSWORD', 'admin');
+        $errorMessages = [];
 
-    session_start();
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-      $login = $_POST["login"];
-      $password = $_POST["password"];
+        session_start();
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $login = $_POST["login"];
+            $password = $_POST["password"];
 
-      // Check if form is filled
-      if (!isset($_POST["login"]) || !isset($_POST["password"]) || empty($_POST["login"]) || empty($_POST["password"])) {
-        $errorMessages = ["Please fill in both fields."];
-      } else {
-        // Check if informations are corrects
-        if ($login == "wipa" && $password == "admin") {
-          $_SESSION["user"] = "wipa";
-          header("Location: /");
-          die();
-        } else {
-          $errorMessages = ["Invalid username or password."];
+          // Check if form is filled
+            if (!isset($_POST["login"]) || empty($_POST["login"])) {
+                $errorMessages = ["Please fill in login fields."];
+            } elseif (!isset($_POST["password"]) || empty($_POST["password"])) {
+                $errorMessages = ["Please fill in password field."];
+            } else {
+              // Check if informations are corrects
+                if ($login == "wipa" && $password == "admin") {
+                    $_SESSION["user"] = "wipa";
+                    header("Location: /");
+                    die();
+                } else {
+                    $errorMessages = ["Invalid username or password."];
+                }
+            }
         }
-      }
+        return $this->twig->render('Log/login.html.twig', ['errorMessages' => $errorMessages]);
     }
-    return $this->twig->render('Log/login.html.twig', ['errorMessages' => $errorMessages]);
-  }
 }
