cc2c378a5d82c42af33d4bf112107ba521a5b71f
MisterPingouin <[anthony.axeldisplay@gmail.com](mailto:anthony.axeldisplay@gmail.com)>
2023-04-19 10:17:32 +0200

    Add date on pic / Small fixed on css / Add title on page / updated BDD

--- src/Model/PhotoManager.php
+++ src/Model/PhotoManager.php
@@ -2,30 +2,35 @@
 
 namespace App\Model;
 
+use DateTimeZone;
+use DateTime;
 use PDO;
 
 class PhotoManager extends AbstractManager
 {
     public const TABLE = 'photo';
-
     /**
-     * Insert new item in database
+     * Insert new photo in database
      */
-    public function insert(array $item): int
+    public function insert(array $item): void
     {
-        $dbFields = '(`photo_title`, `photo_link`, `photo_prompt`, `photo_description`)';
-        $placeholderFields = '(:photo_title, :photo_link, :photo_prompt, :photo_description)';
+        // get current time
+        $atz = 'Europe/Paris';
+        $timestamp = time();
+        $adt = new DateTime("now", new DateTimeZone($atz)); //first argument "must" be a string
+        $adt->setTimestamp($timestamp); //adjust the object to correct timestamp
+        $formatedDate = $adt->format('Y-m-d H:i:s');
+        // PDO statements
+        $dbFields = '(`photo_title`, `photo_link`, `photo_prompt`, `photo_description`, `photo_date`)';
+        $placeholderFields = '(:photo_title, :photo_link, :photo_prompt, :photo_description, :photo_date)';
         $statement = $this->pdo->prepare("INSERT INTO " . self::TABLE . " $dbFields VALUES $placeholderFields");
         $statement->bindValue('photo_title', $item['title'], PDO::PARAM_STR);
         $statement->bindValue('photo_link', $item['picture'], PDO::PARAM_STR);
         $statement->bindValue('photo_prompt', $item['prompt'], PDO::PARAM_STR);
         $statement->bindValue('photo_description', $item['description'], PDO::PARAM_STR);
-
+        $statement->bindValue('photo_date', $formatedDate, PDO::PARAM_STR);
         $statement->execute();
-        return (int)$this->pdo->lastInsertId();
     }
-
-
     /**
      * Update item in database
      */
@@ -34,7 +39,6 @@ class PhotoManager extends AbstractManager
         $statement = $this->pdo->prepare("UPDATE " . self::TABLE . " SET `title` = :title WHERE id=:id");
         $statement->bindValue('id', $item['id'], PDO::PARAM_INT);
         $statement->bindValue('title', $item['title'], PDO::PARAM_STR);
-
         return $statement->execute();
     }
 }
