245413a89289554e23c7acec043a45f098aff61e
MisterPingouin <anthony.axeldisplay@gmail.com>
2023-04-21 10:24:32 +0200

    Small Fix / Upt BDD

--- public/assets/js/jsaddformpreview.js
+++ public/assets/js/jsaddformpreview.js
@@ -1,14 +1,14 @@
 function previewImage() {
-	var url = document.getElementById("url").value;
-	var img = document.createElement("img");
+	let url = document.getElementById("url").value;
+	let img = document.createElement("img");
 	img.style.width = "100px";
 	img.style.height = "100px";
-	var div = document.getElementById("photo");
+	let div = document.getElementById("photo");
 	div.innerHTML = "";
 	img.src = url;
 	div.appendChild(img)
 
 }
 
-var input = document.getElementById("url");
+let input = document.getElementById("url");
 input.addEventListener("input", previewImage);
