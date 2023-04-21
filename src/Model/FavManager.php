abf0142ee48c27b9af350c11a5db88884bfe3018
MisterPingouin <[anthony.axeldisplay@gmail.com](mailto:anthony.axeldisplay@gmail.com)>
2023-04-21 14:29:57 +0200

    Fix US02

--- src/Model/FavManager.php
+++ src/Model/FavManager.php
@@ -8,7 +8,7 @@ class FavManager extends AbstractManager
 {
     public const TABLE = 'fav_photo';
 
-    public function insertfav(int $idfav): void
+    public function insertfav(int $idfav, int $userId): void
     {
         $dbFields = '(`fav_photo_id`,`fav_user_id`)';
         $placeholderFields = '(:fav_photo_id, :fav_user_id)';
@@ -17,14 +17,14 @@ class FavManager extends AbstractManager
         // request to test if a photo is already a fav
         $statement = $this->pdo->prepare("SELECT * FROM " . static::TABLE . $whereFields);
         $statement->bindValue('fav_photo_id', $idfav, PDO::PARAM_INT);
-        $statement->bindValue('fav_user_id', 1, PDO::PARAM_INT);
+        $statement->bindValue('fav_user_id', $userId, PDO::PARAM_INT);
         $statement->execute();
         $isFav = $statement->fetch();
         // if photo is not already a favorite => add it as favorite
         if (!$isFav) {
             $statement = $this->pdo->prepare("INSERT INTO " . self::TABLE . " $dbFields VALUES $placeholderFields");
             $statement->bindValue('fav_photo_id', $idfav, PDO::PARAM_INT);
-            $statement->bindValue('fav_user_id', 1, PDO::PARAM_INT);
+            $statement->bindValue('fav_user_id', $userId, PDO::PARAM_INT);
             $statement->execute();
         }
     }
