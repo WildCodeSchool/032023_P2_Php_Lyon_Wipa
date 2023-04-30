a66836488a175709c487181574d856d67d4a2911
l00ma <looma@protonmail.ch>
2023-04-30 21:41:50 +0200

    followers mecanism is added but at this point not spread in twig pages

--- src/Model/FavManager.php
+++ src/Model/FavManager.php
@@ -8,7 +8,7 @@ class FavManager extends AbstractManager
 {
     public const TABLE = 'fav_photo';
 
-    public function insertFav(int $idfav, int $userId): void
+    public function insertFav(int $idFav, int $userId): void
     {
         $dbFields = '(`photo_id`,`user_id`)';
         $placeholderFields = '(:photo_id, :user_id)';
@@ -16,25 +16,24 @@ class FavManager extends AbstractManager
 
         // request to test if a photo is already a fav
         $statement = $this->pdo->prepare("SELECT * FROM " . self::TABLE . $whereFields);
-        $statement->bindValue('photo_id', $idfav, PDO::PARAM_INT);
+        $statement->bindValue('photo_id', $idFav, PDO::PARAM_INT);
         $statement->bindValue('user_id', $userId, PDO::PARAM_INT);
         $statement->execute();
         $isFav = $statement->fetch();
+
+        // if photo is a favorite => delete from favorites
+        $statement = $this->pdo->prepare("DELETE FROM " . self::TABLE . $whereFields);
+
         // if photo is not already a favorite => add it as favorite
         if (!$isFav) {
             $statement = $this->pdo->prepare("INSERT INTO " . self::TABLE . " $dbFields VALUES $placeholderFields");
-            $statement->bindValue('photo_id', $idfav, PDO::PARAM_INT);
-            $statement->bindValue('user_id', $userId, PDO::PARAM_INT);
-            $statement->execute();
         }
-        // if photo is a favorite => delete from favorites
-        if ($isFav) {
-            $statement = $this->pdo->prepare("DELETE FROM " . self::TABLE . $whereFields);
-            $statement->bindValue('photo_id', $idfav, PDO::PARAM_INT);
-            $statement->bindValue('user_id', $userId, PDO::PARAM_INT);
-            $statement->execute();
-        }
+
+        $statement->bindValue('photo_id', $idFav, PDO::PARAM_INT);
+        $statement->bindValue('user_id', $userId, PDO::PARAM_INT);
+        $statement->execute();
     }
+
     public function selectUserFavs(int $userId): array
     {
         $query = "SELECT fav_photo.photo_id AS id
