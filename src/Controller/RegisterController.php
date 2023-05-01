0dbb3ae6b724de35a93d745fda2b0e113e7e4312
l00ma <looma@protonmail.ch>
2023-05-01 17:32:40 +0200

    followers mecanism is spread in twig profil page. You can see users you follow and stop following them. TODO: add a link to see followed user photos.

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -13,9 +13,6 @@ class RegisterController extends AbstractController
             exit();
         }
 
-        $errors = [];
-        $success = [];
-
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             // Clean $_POST
             $data = array_map('trim', $_POST);
@@ -23,38 +20,40 @@ class RegisterController extends AbstractController
             $password1 = $data['password1'];
             $password2 = $data['password2'];
 
-            $this->validatePassword($password1, $password2, $errors);
-
+            $this->validatePassword($password1, $password2);
 
             // Check if form is filled
             if (!isset($data['username']) || empty($data['username'])) {
-                $errors[] = 'Please fill in username field.';
+                $this->errors[] = 'Please fill in username field.';
             }
 
-            if (empty($errors)) {
+            if (empty($this->errors)) {
                 $username = $data['username'];
                 $password = $data['password1'];
 
                 $registerManager = new RegisterManager();
                 $registerManager->insert($username, $password);
-                $success[] = 'Registration successfull. Please login.';
+                $this->successes[] = 'Registration successfull. Please login.';
             }
-            return $this->twig->render('Register/register.html.twig', ['errors' => $errors, 'success' => $success]);
+            return $this->twig->render('Register/register.html.twig', [
+                'errors' => $this->errors,
+                'successes' => $this->successes
+            ]);
         }
         return $this->twig->render('Register/register.html.twig');
     }
 
-    public function validatePassword(string $password1 = null, string $password2 = null, array &$errors): void
+    public function validatePassword(string $password1 = null, string $password2 = null): void
     {
         // Compare password1 and password2
         if (!isset($password1) || empty($password1)) {
-            $errors[] = 'Please fill in password field.';
+            $this->errors[] = 'Please fill in password field.';
         }
         if (!isset($password2) || empty($password2)) {
-            $errors[] = 'Please retype your password field.';
+            $this->errors[] = 'Please retype your password field.';
         }
         if ($password1 !== $password2) {
-            $errors[] = 'Passwords are not the same.';
+            $this->errors[] = 'Passwords are not the same.';
         }
     }
 }
