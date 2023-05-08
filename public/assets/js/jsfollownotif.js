35a6a5b60d9264a8e0ef6e3d7414609120153b8f
l00ma <looma@protonmail.ch>
2023-05-08 18:43:19 +0200

    add js fetch fct on following system on index.html.twig

---
+++ public/assets/js/jsfollownotif.js
@@ -0,0 +1,40 @@
+const followForms = document.querySelectorAll(".bi-dash-square-fill, .bi-plus-square-fill");
+
+for (let i = 0; i < followForms.length; i++) {
+    let followForm = followForms[i]
+
+    followForm.addEventListener('click', function (event) {
+        let photoUser = this.dataset.photoUser;
+        let photoUserId = this.dataset.photoUserId;
+
+        fetch("user/follow",
+            {
+                method: "POST",
+                //datas posted with a key-value pair
+                body: "followedId=" + photoUserId,
+                headers:
+                    { "Content-Type": "application/x-www-form-urlencoded" }
+
+            }).then((response) => {
+                //start following this user
+                if (followForm.className.match(/plus/g)) {
+                    for (form of followForms) {
+                        if (form.dataset.photoUserId == photoUserId) {
+                            console.log(form.dataset.photoUserId + ' ' + photoUserId);
+                            form.className = "bi bi-dash-square-fill";
+                        }
+                    };
+                    alert("You are following " + photoUser);
+                }
+                //stop following this user
+                else if (followForm.className.match(/dash/g)) {
+                    for (form of followForms) {
+                        if (form.dataset.photoUserId == photoUserId) {
+                            form.className = "bi bi-plus-square-fill";
+                        }
+                    };
+                    alert("You stopped following " + photoUser);
+                }
+            });
+    });
+}
