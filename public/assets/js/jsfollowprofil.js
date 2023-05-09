44539ef2ea0685c5d00910d2e243561955ad2a54
l00ma <looma@protonmail.ch>
2023-05-09 11:11:31 +0200

    alert message active but ugly

--- public/assets/js/jsfollowprofil.js
+++ public/assets/js/jsfollowprofil.js
@@ -1,4 +1,5 @@
 const followForms = document.querySelectorAll(".bi-dash-square-fill, .bi-plus-square-fill");
+const notification = document.getElementById("notification");
 
 const refreshFollowing = document.getElementById("contact-tab");
 if (typeof refreshFollowing !== 'undefined') {
@@ -6,8 +7,6 @@ if (typeof refreshFollowing !== 'undefined') {
         window.location.reload();
     });
 }
-
-
 
 for (let i = 0; i < followForms.length; i++) {
     let followForm = followForms[i]
@@ -33,7 +32,10 @@ for (let i = 0; i < followForms.length; i++) {
                             form.className = "bi bi-dash-square-fill";
                         }
                     };
-                    alert("You are following " + photoUser);
+                    notification.innerHTML = `<div class="alert"><div class="w3-panel w3-green">
+                    <h3>Success!</h3>
+                    <p>You are following ${photoUser}.</p>
+                  </div></div>`;
                 }
                 //stop following this user
                 else if (followForm.className.match(/dash/g)) {
@@ -42,7 +44,10 @@ for (let i = 0; i < followForms.length; i++) {
                             form.className = "bi bi-plus-square-fill";
                         }
                     };
-                    alert("You stopped following " + photoUser);
+                    notification.innerHTML = `<div class="alert"><div class="w3-panel w3-green">
+                    <h3>Success!</h3>
+                    <p>You stopped following ${photoUser}.</p>
+                  </div></div>`;
                 }
             });
     });
